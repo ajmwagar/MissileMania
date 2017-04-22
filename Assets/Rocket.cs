8fee92f27eb22fd64e19ed2b8c5a75649e455704
Salmagundi <ajmw.subs@gmail.com>
2017-04-22 11:35:28 -0700

    Updated Homing Rocket Script

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     public KeyCode DestroyKey = KeyCode.Alpha1;
-	
-	// Update is called once per frame
-	void Update () {
+
+    bool isHoming = false;
+
+    public GameObject Target;
+    public float speed;
+    public Rigidbody rb;
+
+    private void Awake()
+    {
+       rb = gameObject.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update () {
 		if(Input.GetKeyUp(DestroyKey))
         {
             RocketFactory.Instance.DestroyRocket(gameObject);
         }
 	}
+
+    private void FixedUpdate()
+    {
+
+        if(isHoming)
+        {
+                      
+            rb.velocity = (Target.transform.position - transform.position).normalized * speed;
+            // track player
+        }    }
 }
