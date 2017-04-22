a11650af170a9711a8e4350fa34bdde8ccc7a3da
Brint E. Kriebel <github@bekit.net>
2017-04-22 02:17:13 -0700

    Update scene

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger!!! " + other.gameObject.tag);
+        if (other.gameObject.tag == "Projectile")
+        {
+            RocketFactory.Instance.DestroyRocket(other.gameObject);
+        }
+    }
+}
