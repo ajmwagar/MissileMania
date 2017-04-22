0964268ad45a96f00939e60a6fbfae7e80a84adb
Brint E. Kriebel <github@bekit.net>
2017-04-22 14:10:19 -0700

    controller: Use sonic screwdriver model

---
+++ Assets/ScrewdriverHead.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewdriverHead : MonoBehaviour {
+	public Vector3 screwSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(screwSpeed);
+	}
+}
