535f4e9c0818c180576a341d16cba920a88b541f
tiagosomda <tsomda@gmail.com>
2017-04-23 01:38:44 -0700

    added game music

--- Assets/GameMusic.cs
+++ Assets/GameMusic.cs
@@ -8,6 +8,9 @@ public class GameMusic : MonoBehaviour
     private static GameMusic _instance;
     private static GameMusic Instance { get { return _instance; } }
 
+    [Range(0.5f, 1f)]
+    public float StartPlayingAtSec;
+
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -18,6 +21,11 @@ public class GameMusic : MonoBehaviour
         {
             _instance = this;
         }
+    }
+
+    private void Start()
+    {
+        GameMusicLevels[0].Play();
     }
 
     public static void SetGameMusicLevel(int level)
@@ -37,6 +45,23 @@ public class GameMusic : MonoBehaviour
 
     public void Update()
     {
+        if(GameMusicLevels[0].time > StartPlayingAtSec)
+        {
+            return;
+        }
+
         // do turn on/off correct music level at the beginning of the music loop
+        for (int i = 1; i < GameMusicLevels.Length -1; i++)
+        {
+            if(i <= currentLevel)
+            {
+                GameMusicLevels[i].Play();
+                GameMusicLevels[i].time = GameMusicLevels[0].time;
+            }
+            else
+            {
+                GameMusicLevels[i].Stop();
+            }
+        }
     }
 }
