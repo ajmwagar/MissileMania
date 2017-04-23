5f7ced4f3ef1e47af6ab5351c53bcc3ef6b7462c
Xandon <xandon@hotmail.com>
2017-04-23 14:02:57 -0700

    fixed turret Vision4200

---
+++ Assets/Xandon_sandbox/Scripts/TurretHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretHealth : MonoBehaviour {
+
+	public float turretHealth = 100f;
+
+	public void TakeDamage(){
+		turretHealth -= 20f;
+		if (turretHealth < 0f) {
+			KillTurret ();
+			turretHealth = 0f;
+		}
+	}
+
+
+	private void KillTurret() {
+
+
+	}
+
+
+	public void RepairTurret(){
+		if (turretHealth < 100f) {
+			turretHealth += 10f;
+			if (turretHealth >= 100f) {
+				TurretRepaired ();
+				turretHealth = 100f;
+			}
+		}
+	}
+
+	private void TurretRepaired (){
+
+
+	}
+
+}
