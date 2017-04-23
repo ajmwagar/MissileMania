5aab3ee52a5b5991a57fa150e90e7d942397dcbf
Salmagund1 <ajmw.subs@gmail.com>
2017-04-22 18:44:07 -0700

    Add RocketBoom script

---
+++ Assets/RocketBoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBoom : MonoBehaviour {
+    public GameObject BoomStick;
+
+	// Use this for initialization
+	public void explodeRocket()
+    {
+        BoomStick.SetActive(true);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
