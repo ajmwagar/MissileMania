1b5898a1b46a291db27a2b47246e7e2de5e8ad49
Xandon Frogget <xandon@hotmail.com>
2017-04-25 23:38:29 -0700

    TurretDamage xandon_scene_v4.5
    Added collider and audio damage to turret

---
+++ Assets/Xandon_sandbox/Scripts/CrashSound.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrashSound : MonoBehaviour
+{
+
+    public AudioClip crashSoft;
+    public AudioClip crashHard;
+
+
+    private AudioSource source;
+    private float lowPitchRange = .75F;
+    private float highPitchRange = 1.5F;
+    private float velToVol = .2F;
+    private float velocityClipSplit = 10F;
+
+
+    void Awake()
+    {
+
+        source = GetComponent<AudioSource>();
+    }
+
+
+    void OnCollisionEnter(Collision coll)
+    {
+        source.pitch = Random.Range(lowPitchRange, highPitchRange);
+        float hitVol = coll.relativeVelocity.magnitude * velToVol;
+        if (coll.relativeVelocity.magnitude < velocityClipSplit)
+            source.PlayOneShot(crashSoft, hitVol);
+        else
+            source.PlayOneShot(crashHard, hitVol);
+    }
+
+}
