aa824139b511b0ed0478768d7cc12a6773e06fe7
Brint E. Kriebel <github@bekit.net>
2017-04-27 16:56:21 -0700

    demotweaks: Update indexer and add floor adjustment
    Press I to activate indexer
    Up arrow to move floor up
    Down arrow to move floor down

--- Assets/TrackedIndexer.cs
+++ Assets/TrackedIndexer.cs
@@ -1,33 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class TrackedIndexer : MonoBehaviour {
     public bool IndexerEnabled;
     public TextMesh IndexerText;
-    public GameObject UndefinedTrackers;
+    public GameObject UndefinedPrefab;
+    public GameObject CameraRig;
+    public SteamVR_TrackedObject SonicScrewdriver;
     public SteamVR_TrackedObject BatTroller;
     public SteamVR_TrackedObject TrackedTurret;
     public SteamVR_TrackedObject TrackedShield;
 
-    int currentDevice = 3;
+    int currentDevice;
+    int totalDevices;
 
     void OnEnable()
     {
+        currentDevice = 1;
+        totalDevices = 0;
+
         if (IndexerEnabled)
         {
+            for (int i = 0; i < SteamVR.connected.Length; i++)
+            {
+                if (SteamVR.connected[i])
+                {
+                    var deviceClass = OpenVR.System.GetTrackedDeviceClass((uint)i);
+                    if (deviceClass == ETrackedDeviceClass.Controller ||
+                        deviceClass == ETrackedDeviceClass.GenericTracker)
+                    {
+                        GameObject trackedDevice = Instantiate(UndefinedPrefab);
+                        trackedDevice.transform.parent = CameraRig.transform;
+                        trackedDevice.SetActive(true);
+                        trackedDevice.BroadcastMessage("SetDeviceIndex", (int)i, SendMessageOptions.DontRequireReceiver);
+                        totalDevices++;
+                    }
+                }
+            }
+
+            SonicScrewdriver.index = SteamVR_TrackedObject.EIndex.None;
             BatTroller.index = SteamVR_TrackedObject.EIndex.None;
             TrackedTurret.index = SteamVR_TrackedObject.EIndex.None;
             TrackedShield.index = SteamVR_TrackedObject.EIndex.None;
 
+            SonicScrewdriver.gameObject.SetActive(false);
             BatTroller.gameObject.SetActive(false);
             TrackedTurret.gameObject.SetActive(false);
             TrackedShield.gameObject.SetActive(false);
 
-            UndefinedTrackers.SetActive(true);
-        } else
+            IndexerText.text = "Place Bat";
+        }
+        else
         {
-            UndefinedTrackers.SetActive(false);
             gameObject.SetActive(false);
         }
     }
@@ -46,7 +72,7 @@ public class TrackedIndexer : MonoBehaviour {
 
             switch (currentDevice)
             {
-                case 3:
+                case 1:
                     Debug.Log("Setting bat to: " + trackedObjectIndex);
                     BatTroller.gameObject.SetActive(true);
                     BatTroller.index = trackedObjectIndex;
@@ -54,7 +80,7 @@ public class TrackedIndexer : MonoBehaviour {
                     IndexerText.text = "Place Turret";
                     currentDevice++;
                     break;
-                case 4:
+                case 2:
                     Debug.Log("Setting turret to: " + trackedObjectIndex);
                     TrackedTurret.gameObject.SetActive(true);
                     TrackedTurret.index = trackedObjectIndex;
@@ -62,13 +88,28 @@ public class TrackedIndexer : MonoBehaviour {
                     IndexerText.text = "Place Shield";
                     currentDevice++;
                     break;
-                case 5:
+                case 3:
                     Debug.Log("Setting shield to: " + trackedObjectIndex);
                     TrackedShield.gameObject.SetActive(true);
                     TrackedShield.index = trackedObjectIndex;
                     trackedObject.SetActive(false);
-                    gameObject.SetActive(false);
+                    IndexerText.text = "Place Sonic Screwdriver";
+                    currentDevice++;
                     break;
+                case 4:
+                    Debug.Log("Setting Sonic to: " + trackedObjectIndex);
+                    SonicScrewdriver.gameObject.SetActive(true);
+                    SonicScrewdriver.index = trackedObjectIndex;
+                    trackedObject.SetActive(false);
+                    currentDevice++;
+                    break;
+            }
+
+            Debug.Log("Device " + currentDevice + "of " + totalDevices + "indexed.");
+            if (currentDevice > totalDevices)
+            {
+                gameObject.SetActive(false);
+                enabled = false;
             }
         }
     }
